35a93f96200338107679343abaab31d4545be362
Leandro Chaves <leandrochaves.f@gmail.com>
2023-08-25 15:20:03 -0300

    feat: persist exchange to database

--- server/main.go
+++ server/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type ExchangeData struct {
@@ -16,24 +19,18 @@ type Exchange struct {
 	USDBRL ExchangeData `json:"USDBRL"`
 }
 
+type ExchangeDb struct {
+	ID  int `gorm:"primaryKey"`
+	Bid string
+	gorm.Model
+}
+
 func main() {
-
-	// O endpoint necessário gerado pelo server.go para este desafio será: /cotacao e a porta a ser utilizada pelo servidor HTTP será a 8080.
-
 	http.HandleFunc("/cotacao", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// O server.go deverá consumir a API contendo o câmbio de Dólar e Real no
-	// endereço: https://economia.awesomeapi.com.br/json/last/USD-BRL e em seguida
-	// deverá retornar no formato JSON o resultado para o cliente.
-
-	// Usando o package "context", o server.go deverá registrar no banco de dados
-	// SQLite cada cotação recebida, sendo que o timeout máximo para chamar a API
-	// de cotação do dólar deverá ser de 200ms e o timeout máximo para conseguir
-	// persistir os dados no banco deverá ser de 10ms.
-
 	// Check route
 	if r.URL.Path != "/cotacao" {
 		w.WriteHeader(http.StatusNotFound)
@@ -53,18 +50,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Save in database
-	// TODO
+	error = saveToDatabase(exchange)
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if error == context.DeadlineExceeded {
+		println("Timeout")
+		w.WriteHeader(http.StatusRequestTimeout)
+		return
+	}
 
 	// Write response
 	body, error := json.Marshal(exchange.USDBRL)
 	if error != nil {
-		panic(error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
-
 }
 
 func getExchange() (*Exchange, error) {
@@ -72,8 +78,8 @@ func getExchange() (*Exchange, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancel()
 
-	// req, error := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
-	req, error := http.NewRequestWithContext(ctx, "GET", "http://localhost:3001/USD-BRL", nil)
+	req, error := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	// req, error := http.NewRequestWithContext(ctx, "GET", "http://localhost:3001/USD-BRL", nil)
 	if error != nil {
 		return nil, error
 	}
@@ -100,4 +106,25 @@ func getExchange() (*Exchange, error) {
 	}
 
 	return &exchange, nil
+}
+
+func saveToDatabase(exchange *Exchange) error {
+	// add timeout after 10 miliseconds
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	db, err := gorm.Open(sqlite.Open("exchangeDb.sqlite"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	// db.AutoMigrate(&ExchangeDb{})
+
+	// Create
+	db.Create(&ExchangeDb{
+		Bid: exchange.USDBRL.Bid,
+	})
+
+	return ctx.Err()
 }
