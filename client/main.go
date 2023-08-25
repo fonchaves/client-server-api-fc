8aced308be76a890b8ed8b9c1babc33235c49425
Leandro Chaves <leandrochaves.f@gmail.com>
2023-08-25 14:53:45 -0300

    feat: add jsonserver and return data to client

--- client/main.go
+++ client/main.go
@@ -9,9 +9,16 @@ import (
 )
 
 func main() {
+	// O client.go deverá realizar uma requisição HTTP no server.go solicitando a cotação do dólar.
+
+	//O client.go precisará receber do server.go apenas o valor atual do câmbio (campo "bid" do JSON).
+	// Utilizando o package "context", o client.go terá um timeout máximo de 300ms para receber o resultado do server.go.
+
+	// O client.go terá que salvar a cotação atual em um arquivo "cotacao.txt" no formato: Dólar: {valor}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
 		panic(err)
 	}
--- server/main.go
+++ server/main.go
@@ -1,28 +1,103 @@
 package main
 
 import (
-	"log"
+	"context"
+	"encoding/json"
+	"io"
 	"net/http"
 	"time"
 )
 
+type ExchangeData struct {
+	Bid string `json:"bid"`
+}
+
+type Exchange struct {
+	USDBRL ExchangeData `json:"USDBRL"`
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+
+	// O endpoint necessário gerado pelo server.go para este desafio será: /cotacao e a porta a ser utilizada pelo servidor HTTP será a 8080.
+
+	http.HandleFunc("/cotacao", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	log.Println("Request iniciada")
-	defer log.Println("Request finalizada")
-	select {
-	case <-time.After(5 * time.Second):
-		// Imprime no comand line stdout
-		log.Println("Request processada com sucesso")
-		// Imprime no browser
-		w.Write([]byte("Request processada com sucesso"))
-	case <-ctx.Done():
-		// Imprime no comand line stdout
-		log.Println("Request cancelada pelo cliente")
+	// O server.go deverá consumir a API contendo o câmbio de Dólar e Real no
+	// endereço: https://economia.awesomeapi.com.br/json/last/USD-BRL e em seguida
+	// deverá retornar no formato JSON o resultado para o cliente.
+
+	// Usando o package "context", o server.go deverá registrar no banco de dados
+	// SQLite cada cotação recebida, sendo que o timeout máximo para chamar a API
+	// de cotação do dólar deverá ser de 200ms e o timeout máximo para conseguir
+	// persistir os dados no banco deverá ser de 10ms.
+
+	// Check route
+	if r.URL.Path != "/cotacao" {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
+
+	// Get exchange
+	exchange, error := getExchange()
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if error == context.DeadlineExceeded {
+		println("Timeout")
+		w.WriteHeader(http.StatusRequestTimeout)
+		return
+	}
+
+	// Save in database
+	// TODO
+
+	// Write response
+	body, error := json.Marshal(exchange.USDBRL)
+	if error != nil {
+		panic(error)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+
+}
+
+func getExchange() (*Exchange, error) {
+	// add timeout after 200 miliseconds
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	// req, error := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	req, error := http.NewRequestWithContext(ctx, "GET", "http://localhost:3001/USD-BRL", nil)
+	if error != nil {
+		return nil, error
+	}
+
+	// Do the request
+	resp, error := http.DefaultClient.Do(req)
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+
+	// Read all contents
+	body, error := io.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+	println(string(body))
+
+	// Unmarshal
+	var exchange Exchange
+	error = json.Unmarshal(body, &exchange)
+	if error != nil {
+		return nil, error
+	}
+
+	return &exchange, nil
 }
